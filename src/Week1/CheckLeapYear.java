d23cd4ddd19de64486b69582b5e073362640909e
madhavmadupu <134821170+madhavmadupu@users.noreply.github.com>
2023-12-18 20:53:07 +0530

    changes

--- src/Week1/CheckLeapYear.java
+++ src/Week1/CheckLeapYear.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class CheckLeapYear {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Scanner sc=new Scanner(System.in);
 		System.out.println("Enter a Year:");
 		int year=sc.nextInt();
